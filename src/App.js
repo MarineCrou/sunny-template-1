4fc5f1ab5349fee34213d0116c10ab1c602ca8bb
Marine Crouzet <marine.m.crouzet@gmail.com>
2025-03-02 19:36:01 +0100

    first commit + set-up tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="bg-orange-400">Hello</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
