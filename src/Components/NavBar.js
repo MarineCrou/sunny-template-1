1c3dd47dc70b8d72ac799e45e0f40c65aff0bea5
Marine Crouzet <marine.m.crouzet@gmail.com>
2025-03-03 12:55:02 +0100

    fixed issue in nav, with Link

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { Link } from "react-router-dom"; // Import Link
+import { Link } from "react-router-dom";
 
 const navigation = [
   { name: "Home", href: "/" },
@@ -13,6 +13,7 @@ const navigation = [
 
 export default function NavBar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
     <>
       <header className="absolute inset-x-0 top-0 z-50">
@@ -21,7 +22,7 @@ export default function NavBar() {
           className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         >
           <div className="flex lg:flex-1">
-            <Link href="/" className="-m-1.5 p-1.5">
+            <Link to="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img
                 alt=""
@@ -44,7 +45,7 @@ export default function NavBar() {
             {navigation.map((item) => (
               <Link
                 key={item.name}
-                href={item.href}
+                to={item.href}
                 className="text-sm/6 font-semibold text-gray-900"
               >
                 {item.name}
@@ -52,7 +53,7 @@ export default function NavBar() {
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <Link href="/" className="text-sm/6 font-semibold text-orange-600">
+            <Link to="/" className="text-sm/6 font-semibold text-orange-600">
               Donations <span aria-hidden="true">&rarr;</span>
             </Link>
           </div>
@@ -65,7 +66,7 @@ export default function NavBar() {
           <div className="fixed inset-0 z-50" />
           <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <Link href="/" className="-m-1.5 p-1.5">
+              <Link to="/" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
                 <img
                   alt=""
@@ -88,7 +89,7 @@ export default function NavBar() {
                   {navigation.map((item) => (
                     <Link
                       key={item.name}
-                      href={item.href}
+                      to={item.href}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                     >
                       {item.name}
@@ -97,7 +98,7 @@ export default function NavBar() {
                 </div>
                 <div className="py-6">
                   <Link
-                    href="/"
+                    to="/"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                   >
                     Log in
